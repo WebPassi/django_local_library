ca6ed70cab65bf0b902e023fbf496e24bc3e55b9
Pasquale Franz <WebPassi>
2018-01-19 20:55:48 +0100

    Added files and changes required for deployment to heroku

--- aufgabengenerator/urls.py
+++ aufgabengenerator/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+app_name="generator"
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'aufgabengenerator.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^generator/', include('generator.urls', namespace="generator")),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^generator/', include('generator.urls', namespace="generator")),
+    #url(r'^admin/', include(admin.site.urls)),
 ]
